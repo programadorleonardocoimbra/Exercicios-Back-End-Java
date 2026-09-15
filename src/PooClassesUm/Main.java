511ef9d520297aa5cc56a92bdbaae3f7b74972a8
Leonardo Coimbra <programadorleonardocoimbra@gmail.com>
2026-09-14 23:05:52 -0300

    Atualização de Repositório

---
+++ src/PooClassesUm/Main.java
@@ -0,0 +1,23 @@
+package PooClassesUm;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var male = new Person();
+        male.setName("João") ;
+        male.setAge(12);
+        var female = new Person();
+        female.setName("Maria");
+        female.setAge(10);
+        System.out.println("Male name: " + female.getName() + " age: " + female.getAge());
+        System.out.println("Male name: " + male.getName() + " age: " + male.getAge());
+
+
+
+    }
+
+
+}
---
+++ src/PooClassesUm/Person.java
@@ -0,0 +1,27 @@
+package PooClassesUm;
+
+public class Person {
+
+    private String name;
+
+    private int age;
+
+    public String getName(){
+        return name;
+    };
+
+    public Person setName(String name){
+        this.name = name;
+        return this;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+
+    }
+
+}
